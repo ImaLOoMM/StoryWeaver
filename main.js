f328e5c32df29815f18b634511c2ad9cf56da660
MrRadm1r <rarmir1200363rus@gmail.com>
2024-03-10 14:50:22 +0500

    start

---
+++ main.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const url = require('url');
+const {app, BrowserWindow} = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800, 
+        height: 540,
+        frame: false,
+        icon: __dirname + "/img/swlogo.png"
+    });
+
+    win.setFullScreen(true);
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    win.webContents.openDevTools(); // Dev Tools
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
