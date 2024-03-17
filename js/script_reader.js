8b62cdb53514a6cfca366a82c32aa99b7eeca0d6
MrRadm1r <rarmir1200363rus@gmail.com>
2024-03-17 21:59:52 +0500

    Loading

---
+++ js/script_reader.js
@@ -0,0 +1,42 @@
+const body = document.getElementById('main-body');
+let bodyBg = body.style.backgroundColor
+
+
+function read(script){
+    let key = 0;
+    while (true){
+        const [funcname, kwargs, next] = script[key]
+        console.log(key); 
+        console.log(funcname);
+        console.log(kwargs);
+        console.log(next);
+        if (next in script){
+            key = next
+        } else {
+            console.log("end");
+            return 0;
+        }
+    }
+}
+
+fetch('novels/v3.json') // Загружаем файл JSON
+    .then(response => {
+        // Обработка HTTP-ответа
+        
+        return response.json(); // Преобразуем ответ в JSON и передаем дальше
+    })
+    .then(data => {
+        read(data)
+    })
+    .then(() => {
+        var overlay = document.getElementById("loading-window");
+        if (overlay) {
+            overlay.parentNode.removeChild(overlay);
+        }
+    }
+
+    )
+    .catch(error => {
+        // Обработка ошибок
+        console.error('Ошибка загрузки файла:', error);
+    });
