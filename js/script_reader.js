2d39a1d431a2f652d2f04176a9cb8f025cad0bd0
MrRadm1r <rarmir1200363rus@gmail.com>
2024-04-25 13:53:23 +0500

    preparng

--- js/script_reader.js
+++ js/script_reader.js
@@ -2,15 +2,81 @@ const body = document.getElementById('main-body');
 const loading_img = document.getElementById('loading-progress');
 
 
-function play_gif(frame) {
-    const image = document.getElementById('loading-progress');
-    const randomNumber = Math.floor(Math.random() * 1000000);
-    const newSrc = "img/loading/frame_" + frame + ".png" + '?random=' + randomNumber;
-    image.src = newSrc;
+function fill({area = "content", text="", background=""}, raw_next){
+    return new Promise((resolve, reject) => {
+        let interaction_area = document.getElementById(area); // Определение области заливки
+        if (!interaction_area){
+            console.error("AttributeError: Uncnown area to fill:", area)
+        }
+        if (text){
+            interaction_area.style.color = text;
+        }
+        if (background){
+            interaction_area.style.backgroundColor = background;
+        }
+        if(raw_next.length > 1){
+            console.warn("Too many links")
+        }
+         // Нет никаких вариаций, поэтому выбирается первый элемент во избежание ошибок
+        resolve(raw_next[0])
+    });
+}
+
+function await_click({where = "content"}, raw_next){
+    return new Promise((resolve, reject) => {
+        const onClick = () => {
+             // После выхода из функции обработчик клика больше не пригодится. Удаляем
+            target.removeEventListener('click', onClick);
+             // Нет никаких вариаций, поэтому выбирается первый элемент во избежание ошибок
+            if(raw_next.length > 1){ console.warn("Too many links") }
+            resolve(raw_next[0]);
+        };
+         // Определение области ожидания клика
+        target = document.getElementById(where);
+         // обработчик клика
+        target.addEventListener('click', onClick);
+    });
+}
+
+function text({text, backgroundColor = "", textColor = "", fontSize = "",
+              borderStyle = "", borderColor = "", borderWidth = ""}, raw_next){
+    return new Promise((resolve, reject) => {
+        var paragraph = document.getElementById("text");
+        paragraph.innerHTML = text;
+        if (backgroundColor){ paragraph.style.backgroundColor = backgroundColor }
+        if (textColor){ paragraph.style.color = textColor }
+        if (fontSize){ paragraph.style.fontSize = fontSize }
+        if (borderStyle){ paragraph.style.borderStyle = borderStyle }
+        if (borderColor){ paragraph.style.borderColor = borderColor }
+        if (borderWidth){ paragraph.style.borderWidth = borderWidth }
+         // Нет никаких вариаций, поэтому выбирается первый элемент во избежание ошибок
+        if(raw_next.length > 1){console.warn("Too many links") }
+        resolve(raw_next[0]);
+    });
+}
+
+async function behavior_manager(func_name, func_kwargs, raw_next){
+    return new Promise((resolve, reject) => {
+        let next;
+        setTimeout(async () => {
+            switch(func_name){
+                case "fill":
+                    next = await fill(func_kwargs, raw_next);
+                    break;
+                case "await-click":
+                    next = await await_click(func_kwargs, raw_next);
+                    break;
+                case "text":
+                    next = await text(func_kwargs, raw_next);
+                    break;
+            }
+            resolve(next);
+        }, 1000);
+    });
 }
 
 
-function read(script){
+async function read(script){
     let key = 0;
     let len = Object.keys(script).length;
     let element = 0;
@@ -23,11 +89,15 @@ function read(script){
             console.log(progress + "%")
         }
 
-        const [funcname, kwargs, next] = script[key]
+        const [funcname, kwargs, raw_next] = script[key]
+
+        next = await behavior_manager(funcname, kwargs, raw_next);
+
         // console.log(key); 
-        // console.log(funcname);
-        // console.log(kwargs);
-        // console.log(next);
+        
+        console.log(funcname);
+        console.log(kwargs);
+        console.log(raw_next, next);
         if (next in script){
             key = next
         } else {
@@ -38,7 +108,7 @@ function read(script){
     }
 }
 
-fetch('novels/50.json') // Загружаем файл JSON
+fetch("novels/prototype_v1/behavior.json") // Загружаем файл JSON
     .then(response => {
         // Обработка HTTP-ответа
         return response.json(); // Преобразуем ответ в JSON и передаем дальше
--- renderer.js
+++ renderer.js
@@ -2,19 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
     const panel_btn = document.getElementById('panel_button');
     const panel = document.getElementById('panel');
     const oc_arrow = document.getElementById('oc_arrow');
-    const content = document.getElementById('content')
+    const action_area = document.getElementById("everywhere");
+    // const content = document.getElementById('content')
     let is_options_panel_opened = false;
 
     panel_btn.addEventListener('click', function () {
         if (is_options_panel_opened){
             /*Закрыть окно*/
-            content.style.filter = "none";
+            var elements = document.querySelectorAll('#everywhere *');
+                elements.forEach(function(element) {
+                    element.style.filter = "";
+            });
+
             panel.style.left = '-20%';
             panel_btn.style.left = '0%'
             oc_arrow.classList.remove('left-arrow');
             is_options_panel_opened = false;
         } else {
-            content.style.filter = "blur(10px)"
+            // content.style.filter = "blur(10px)"
+            var elements = document.querySelectorAll('#everywhere *');
+                elements.forEach(function(element) {
+                    element.style.filter = "blur(5px)";
+            });
+            
             panel.style.left = '0px';
             panel_btn.style.left = '20%'
             oc_arrow.classList.add('left-arrow');
